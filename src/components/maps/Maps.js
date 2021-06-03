72841b7d444ed93b73f1e680970592e7d663b8bc
Israel Sanchez <50373290+IsrraelS@users.noreply.github.com>
2021-06-03 18:06:22 +0200

    refactorized maps and fetch api

--- src/components/maps/Maps.js
+++ src/components/maps/Maps.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import  GoogleMaps from 'simple-react-google-maps';
+//import  GoogleMaps from 'simple-react-google-maps';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+//import CurrentLocation from '../../map/map';
 
-class Maps extends Component {
+const mapStyles = {
+  width: '39.9em',
+  height: '19.9em'
+};
+
+/*class Maps extends Component {
+    
     render(){
 
         return(
@@ -14,16 +22,71 @@ class Maps extends Component {
                         lat:40.4127355,
                         lng:-3.695428
                     }}
-                    markers={[
-                        {lat:40.409711, lng: -3.692569},
-                        {lat:40.412072, lng: -3.692569},
-                        {lat:40.451824, lng: -3.692569}
-                    ]}
                 />
+                <button></button>
             </div>
         );
     }
-
+   //}
 }
-export default Maps;
+export default Maps;*/
+
+export class Maps extends Component {
+    state = {
+        showingInfoWindow: false,  // Hides or shows the InfoWindow
+        activeMarker: {},          // Shows the active marker upon click
+        selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
+  };
+
+    onMarkerClick = (props, marker, e) =>
+      this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+    });
+
+    onClose = props => {
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        });
+      }
+    };
+
+   render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat:40.4127355,
+            lng:-3.695428
+          }
+        }
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Current Location'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDtwqqAT436HDAnMtsO65OyYWibfCdJ4YM'
+})(Maps);
+
 
---
+++ src/services/mapAcces.js
@@ -0,0 +1,35 @@
+
+const urlLocal = `http://localhost:3000`;
+
+const mapAcces = {
+
+    registerMarkers : async (lat, lng, id) => {
+        try {
+            let response = await fetch(`${urlLocal}/markers`, 
+            { method: 'POST', 
+            body: JSON.stringify({ 
+                lat: lat,
+                lng: lng}),
+                headers:{'Content-Type': 'application/json'}
+            });
+            response = await response.json();
+            return response;
+        } catch (error) {
+            console.log('Ha habido un problema' + error);
+        }
+    },
+
+    checkMarkers : async () => {
+        try {
+            let response = await fetch(`${urlLocal}/markers/`, 
+            { method: 'GET' });
+            response = await response.json();
+            return response;
+        } catch (error) {
+            console.log('Ha habido un problema' + error);
+        }
+    },
+
+}
+
+export default mapAcces
